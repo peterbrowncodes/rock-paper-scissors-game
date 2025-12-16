cc61b9f0c1b8424d946563bdc153755dc526e232
peterbrowncodes <99191823+peterbrowncodes@users.noreply.github.com>
2025-12-15 22:22:16 -0500

    Updates JS script

--- script.js
+++ script.js
@@ -6,81 +6,115 @@
 - Function that starts a new game round - playRound
 */
 
-let humanScore = 0
-let computerScore = 0
-let computerChoice = ''
-let humanChoice = ''
+/* VARIABLES */
+let humanScore = 0;
+let computerScore = 0;
+let computerChoice = "";
+let humanChoice = "";
+let result = ""
+let roundCounter = 0;
 
-function getComputerChoice(choice) {
-    let result = Math.floor(Math.random() * 3)
+const buttonRock = document.querySelector("#rock");
+const buttonPaper = document.querySelector("#paper");
+const buttonScissors = document.querySelector("#scissors");
+const hScore = document.getElementById("#humanScore");
+const cScore = document.getElementById("#computerScore");
+const hChoice = document.getElementById("#humanChoice");
+const cChoice = document.getElementById("#computerChoice");
+const displayResult = document.getElementById("#result");
+const title = document.getElementById("#title");
 
-    if (result === 0) {
-        choice = 'Rock'
-    } else if (result === 1) {
-        choice = 'Paper'        
+// EVENT LISTENERS
+    if (gameOn) {
+    buttonRock.addEventListener("click", (e) => {
+        humanChoice = getHumanChoice(buttonRock.value);
+        playRound(humanChoice, getComputerChoice())
+    })
+    buttonPaper.addEventListener("click", (e) => {
+        humanChoice = getHumanChoice(buttonPaper.value);
+        playRound(humanChoice, getComputerChoice())
+    })
+    buttonScissors.addEventListener("click", (e) => {
+        humanChoice = getHumanChoice(buttonScissors.value);
+        playRound(humanChoice, getComputerChoice())
+    })    
+}
+
+
+// FUNCTIONS
+
+const getComputerChoice = () => {
+    let cResult = "";
+    let r = Math.floor(Math.random() * 3);
+    if (r === 0) {
+        cResult = 'Rock'
+    } else if (r === 1) {
+        cResult = 'Paper'        
     } else {
-        choice = 'Scissors'
+        cResult = 'Scissors'
     }
-
-    console.log(`Computer picks ${choice}.`)
-    return choice
+    console.log(`Computer picks ${cResult}.`);
+    return cResult;
 } 
 
-function getHumanChoice(choice) {
-    let rpsChoice = prompt('Make your choice; rock, paper or scissors?');
-
-    if (rpsChoice.toLowerCase() === 'rock') {
-        choice = 'Rock'
-    } else if (rpsChoice.toLowerCase() === 'paper') {
-        choice = 'Paper'
-    } else if (rpsChoice.toLowerCase() === 'scissors') {
-        choice = 'Scissors'
+function getHumanChoice(c) {
+    let hResult = "";
+    if (c.toLowerCase() === 'rock') {
+        hResult = 'Rock'
+    } else if (c.toLowerCase() === 'paper') {
+        hResult = 'Paper'
+    } else if (c.toLowerCase() === 'scissors') {
+        hResult = 'Scissors'
     } else {
-        choice = 'Invalid answer'
+        hResult = 'Invalid answer'
     };
-
-    console.log(`You picked ${choice}.`)
-    return choice
+    console.log(`You picked ${c}.`);
+    return hResult;
 }
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log("It's a tie! No points awarded.")
-    } 
-    else if (
-        (humanChoice === 'Rock' && computerChoice === 'Scissors') || 
-        (humanChoice === 'Paper' && computerChoice === 'Rock') || 
-        (humanChoice === 'Scissors' && computerChoice === 'Paper')
-    ) {
-        humanScore ++
-        console.log("You win! 1 point to humans!")
-    } 
-    else {
-        computerScore ++
-        console.log("Computer wins! 1 point to computers!")
+    
+        if (humanChoice === computerChoice) {
+            result = ("It's a tie! No points awarded.")
+        } 
+        else if (
+            (humanChoice === 'Rock' && computerChoice === 'Scissors') || 
+            (humanChoice === 'Paper' && computerChoice === 'Rock') || 
+            (humanChoice === 'Scissors' && computerChoice === 'Paper')
+        ) {
+            humanScore ++
+            result = ("You win! 1 point to humans!")
+        } 
+        else {
+            computerScore ++
+            result = ("Computer wins! 1 point to computers!")
+        }
+    roundCounter ++
+    
+
+    if (humanScore >= 5) {
+        if (humanScore > computerScore) {
+            result = 'Humans win!'
+        }
+        else {
+            result = 'Computers win! Humanity has fallen!'
+        }
     }
 
-    console.log('Human score ' + humanScore)
-    console.log('Computer score ' + computerScore)
-}
+    console.log(humanChoice);
+    console.log(computerChoice);
+    console.log(result);
+    console.log(humanScore);
+    console.log(computerScore);
 
+    
+/* DOM MANIPULATION */
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        console.log('ROUND ' + [i+1])
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection)
-    }
-
-    if (humanScore > computerScore) {
-        console.log('Humans win!')
-    } else if (humanScore === computerScore) {
-        console.log('It\'s a tie! Humanity lives to play another day')
-    } 
-    else {
-        console.log('Computers win! Humanity has fallen!')
-    }
-}
-
-playGame()
+    document.getElementById("title").textContent = `ROUND ${roundCounter}`;
+    document.getElementById("humanScore").textContent = `User Score: ${humanScore}`;
+    document.getElementById("computerScore").textContent = `Computer Score: ${computerScore}`;
+    document.getElementById("humanChoice").textContent = `User Choice: ${humanChoice}`;
+    document.getElementById("computerChoice").textContent = `Computer Choice: ${computerChoice}`;
+    document.getElementById("result").textContent = `Results: ${result}`;
+    return result
+};
